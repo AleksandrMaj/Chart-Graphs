670de2ff1069095c56e2c43f65c394a730867b1d
Aleksandr Maj <aleksandrmaj2001@gmail.com>
2022-10-04 22:04:36 +0200

    Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BackgroundComponent } from './components/background/background.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundComponent
   ],
   imports: [
     BrowserModule
