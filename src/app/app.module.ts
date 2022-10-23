ee527c11029251875f33afcca75fe9e85c9e42ba
Aleks <aleksandrmaj2001@gmail.com>
2022-10-23 13:57:22 +0200

    Added Button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BackgroundComponent } from './components/background/background.component';
+import { ChartsComponent } from './components/charts/charts.component';
+import { AddChartButtonComponent } from './components/add-chart-button/add-chart-button.component';
+import { ChartPartComponent } from './components/chart-part/chart-part.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BackgroundComponent
+    ChartsComponent,
+    AddChartButtonComponent,
+    ChartPartComponent,
   ],
   imports: [
     BrowserModule
